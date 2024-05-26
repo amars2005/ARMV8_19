c06ba815c1409a4e7c64f33b5b103a971e8d93f4
sp3123 <sp3123@ic.ac.uk>
2024-05-26 23:20:42 +0100

    fix: pass state as reference not value, change .gitignore

--- src/emulate.c
+++ src/emulate.c
@@ -36,14 +36,14 @@ typedef struct {
 } state;
 
 // sets the values of memory and registers to 0x0
-static void setup(state cstate) {
-  for (int i; i < MEM_SIZE; i++) { cstate.memory[i] = 0; }
-  for (int i; i < GREG_NUM; i++) { cstate.R[i]      = 0; }
-  cstate.PC       = 0;
-  cstate.PSTATE.Z = 1;
-  cstate.PSTATE.C = 0;
-  cstate.PSTATE.N = 0;
-  cstate.PSTATE.V = 0;
+static void setup(state* cstate) {
+  for (int i; i < MEM_SIZE; i++) { cstate->memory[i] = 0; }
+  for (int i; i < GREG_NUM; i++) { cstate->R[i]      = 0; }
+  cstate->PC       = 0;
+  cstate->PSTATE.Z = 1;
+  cstate->PSTATE.C = 0;
+  cstate->PSTATE.N = 0;
+  cstate->PSTATE.V = 0;
 }
 
 #define VALUE_STR_LENGTH 16
@@ -91,6 +91,6 @@ void outputFile(state cstate, char outputString[]) {
 
 int main(int argc, char **argv) {
   state cstate = { .ZR = 0 }; // initializes the machine
-  setup(cstate);
+  setup(&cstate);
   return EXIT_SUCCESS;
 }
