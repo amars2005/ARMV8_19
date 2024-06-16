5ddcb9a7b9736bb7d3386783c9f7735b1c888e6f
sp3123 <sp3123@ic.ac.uk>
2024-06-16 15:03:29 +0100

    feat: implemented function that performs the first pass

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "symbol_table.h"
+#include <regex.h>
+#include <inttypes.h>
+#include <string.h>
+// #include "symbol_table.h"
 
 #define NEW_SYM_TABLE malloc(sizeof(symbolt))
+#define LABEL_BUFFER 50
 
 typedef struct {
-    char* label;
+    char label[LABEL_BUFFER];
     int   value;
 } LVPair;
 
@@ -17,7 +21,8 @@ struct symbolt{
 typedef struct symbolt *symbolt;
 
 void addToTable(symbolt t, char* label, int value) {
-    LVPair pair = { .label = label, .value = value };
+    LVPair pair = { .value = value };
+    strcpy(pair.label, label);
     while (t->next != NULL) {
         t = t->next;
     }
@@ -28,11 +33,37 @@ void addToTable(symbolt t, char* label, int value) {
 
 int find(symbolt t, char* label) {
     while (t != NULL) {
-        if (label == t->pair.label) {
+        if (strcmp(label, t->pair.label) == 0) {
             return t->pair.value;
         }
         t = t->next;
     }
     fprintf(stderr, "Label: %s not found", label);
     exit(1);
+}
+
+void fist_pass(symbolt t, char** lines) {
+    char* label_regex_str = "[a-zA-Z_.][a-zA-Z0-9$_.]*:";
+    regex_t label_regex;
+    int value = regcomp(&label_regex, label_regex_str, 0);
+
+    if (value != 0) {
+        fprintf(stderr, "Regex compilation failed\n");
+        exit(1);
+    }
+
+    int instr_num = 0;
+    for (char** line = lines; *line != NULL; line ++) {
+        if (regexec(&label_regex, *line, 0, NULL, 0) == REG_NOMATCH) {
+            instr_num ++;
+            continue;
+        }
+
+        char label[strlen(*line)];
+        strcpy(label, *line);
+        label[strlen(label)-1] = '\0';
+        int addr = (instr_num + 1) * 4;
+
+        addToTable(t, label, addr);
+    }
 }
