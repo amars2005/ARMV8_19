2ddf9b6928508fe63e06d21dcdbed34e923d1e6b
sp3123 <sp3123@ic.ac.uk>
2024-06-17 18:24:36 +0100

    feat: logic instruction assembler implemented

--- src/DPI-assembler.c
+++ src/DPI-assembler.c
@@ -22,9 +22,31 @@ uint32_t assembleArithmeticDPI(char* opc, int rd, int rn, int imm12, bool sh, bo
 }
 
 uint32_t assembleWideMoveDPI(char* opc, int rd, int imm16, int hw, bool sf) {
-    uint32_t instr = SF(sf) + DPI + ARITHM_DPI + HW(hw) + IMM16(imm16) + RD(rd);
+    uint32_t instr = SF(sf) + DPI + WMOVE_DPI + HW(hw) + IMM16(imm16) + RD(rd);
     if (strcmp(opc, "movn") == 0)  { instr += OPC(movn); }
     if (strcmp(opc, "movz") == 0)  { instr += OPC(movz); }
     if (strcmp(opc, "movk") == 0)  { instr += OPC(movk); }
+    return instr;
+}
+
+uint32_t assembleArithmeticDPR(char* opc, int rd, int rn, int rm, int shift, int op2, bool sf) {
+    uint32_t instr = SF(sf) + DPR + ARITHM_DPR + SHIFT(shift) + RM(rm) + OP2(op2) + RN(rn) + RD(rd);
+    if (strcmp(opc, "add") == 0)  { instr += OPC(add); }
+    if (strcmp(opc, "adds") == 0) { instr += OPC(adds); }
+    if (strcmp(opc, "sub") == 0)  { instr += OPC(sub); }
+    if (strcmp(opc, "subs") == 0) { instr += OPC(subs); }
+    return instr;
+}
+
+uint32_t assembleLogicDPR(char* opc, int rd, int rn, int rm, int shift, int op2, bool sf) {
+    uint32_t instr = SF(sf) + DPR + LOGIC_DPR + SHIFT(shift) + RM(rm) + OP2(op2)+ RN(rn) + RD(rd);
+    if (strcmp(opc, "and") == 0)  { instr += OPC(andd); }
+    if (strcmp(opc, "bic") == 0)  { instr += OPC(bic) + N; }
+    if (strcmp(opc, "orr") == 0)  { instr += OPC(orr); }
+    if (strcmp(opc, "orn") == 0)  { instr += OPC(orn) + N; }
+    if (strcmp(opc, "eor") == 0)  { instr += OPC(eor); }
+    if (strcmp(opc, "eon") == 0) { instr += OPC(eon) + N; }
+    if (strcmp(opc, "ands") == 0)  { instr += OPC(ands); }
+    if (strcmp(opc, "bics") == 0) { instr += OPC(bics) + N; }
     return instr;
 }
--- src/DPI-assembler.h
+++ src/DPI-assembler.h
@@ -16,13 +16,24 @@
 #define OPC(i)     ((i) << 29)
 #define HW(i)      ((i) << 21)
 #define IMM16(i)   ((i) << 5)
+#define WMOVE_DPI  ((5) << 23)
+#define ARITHM_DPR ( 1  << 24)
+#define DPR        ( 5  << 25)
+#define SHIFT(i)   ((i) << 22)
+#define LOGIC_DPR  ( 0 )
+#define N          ( 1 << 21)
+#define OP2(i)     ((i) << 10)
 
 #define movn 0
 #define movz 2
 #define movk 3
 
 typedef enum { add, adds, sub, subs } arithmeticDPI_t;
+typedef enum { andd, orr, eor, ands } logicDPR_t;
+typedef enum { bic, orn, eon, bics } logicDPRN_t;
 
 uint32_t assembleMultiply(char* opc, int rd, int rn, int rm, int ra, bool sf);
 uint32_t assembleArithmeticDPI(char* opc, int rd, int rn, int imm12, bool sh, bool sf);
 uint32_t assembleWideMoveDPI(char* opc, int rd, int imm16, int hw, bool sf);
+uint32_t assembleArithmeticDPR(char* opc, int rd, int rn, int rm, int shift, int op2, bool sf);
+uint32_t assembleLogicDPR(char* opc, int rd, int rn, int rm, int shift, int op2, bool sf);
