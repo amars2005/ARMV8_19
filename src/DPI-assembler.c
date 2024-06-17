2f9efddf97e310c623f95218db9137aa4a6517fd
sp3123 <sp3123@ic.ac.uk>
2024-06-17 17:19:19 +0100

    feat: immediate data processing instruction assembler implemented

--- src/DPI-assembler.c
+++ src/DPI-assembler.c
@@ -12,3 +12,19 @@ uint32_t assembleMultiply(char* opc, int rd, int rn, int rm, int ra, bool sf) {
     return instr;
 }
 
+uint32_t assembleArithmeticDPI(char* opc, int rd, int rn, int imm12, bool sh, bool sf) {
+    uint32_t instr = SF(sf) + DPI + ARITHM_DPI + SH(sh) + IMM12(imm12) + RN(rn) + RD(rd);
+    if (strcmp(opc, "add") == 0)  { instr += OPC(add); }
+    if (strcmp(opc, "adds") == 0) { instr += OPC(adds); }
+    if (strcmp(opc, "sub") == 0)  { instr += OPC(sub); }
+    if (strcmp(opc, "subs") == 0) { instr += OPC(subs); }
+    return instr;
+}
+
+uint32_t assembleWideMoveDPI(char* opc, int rd, int imm16, int hw, bool sf) {
+    uint32_t instr = SF(sf) + DPI + ARITHM_DPI + HW(hw) + IMM16(imm16) + RD(rd);
+    if (strcmp(opc, "movn") == 0)  { instr += OPC(movn); }
+    if (strcmp(opc, "movz") == 0)  { instr += OPC(movz); }
+    if (strcmp(opc, "movk") == 0)  { instr += OPC(movk); }
+    return instr;
+}
--- src/DPI-assembler.h
+++ src/DPI-assembler.h
@@ -2,12 +2,27 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define RD(i) (i)
-#define RN(i) ((i) << 5)
-#define RA(i) ((i) << 10)
-#define RM(i) ((i) << 16)
-#define X     ((1) << 15)
-#define SF(i) ((i) << 31)
-#define MUL    (27 << 24) // 11011
+#define RD(i)      (i)
+#define RN(i)      ((i) << 5)
+#define RA(i)      ((i) << 10)
+#define RM(i)      ((i) << 16)
+#define X          ((1) << 15)
+#define SF(i)      ((i) << 31)
+#define MUL        (27 << 24) // 11011
+#define DPI        ( 8 << 26)
+#define ARITHM_DPI (2 << 23)
+#define SH(i)      ((i) << 22)
+#define IMM12(i)   ((i) << 10)
+#define OPC(i)     ((i) << 29)
+#define HW(i)      ((i) << 21)
+#define IMM16(i)   ((i) << 5)
+
+#define movn 0
+#define movz 2
+#define movk 3
+
+typedef enum { add, adds, sub, subs } arithmeticDPI_t;
 
 uint32_t assembleMultiply(char* opc, int rd, int rn, int rm, int ra, bool sf);
+uint32_t assembleArithmeticDPI(char* opc, int rd, int rn, int imm12, bool sh, bool sf);
+uint32_t assembleWideMoveDPI(char* opc, int rd, int imm16, int hw, bool sf);
