4484a0b3fa6ce2a0112e23730363d8fd77c4e719
Adam Marshall <am4823@vertex02.doc.ic.ac.uk>
2024-06-19 15:37:18 +0100

    fix: Attempted debugging on assembly file

--- src/led_blink.s
+++ src/led_blink.s
@@ -1,35 +1,40 @@
+.global _start
+_start:
+
 ldr x0, =0x3F200000 
 ldr x3, =0x3F20001C 
 ldr x5, =0x3f200028 
 
 ldr w1, x0          
-movz w2, #0b111, lsl #27                   
+movz w2, #0b11100000000000, lsl #16                  
 bic w1, w1, w2  
-movz w2, #0b001, lsl #27      
+movz w2, #0b00100000000000, lsl #16      
 orr w1, w1, w2
 str w1, [x0]
 
 loop:               
 ldr w1, x3          
-movz w2, #1, lsl #9    
+movz w2, #0x0
+movk w2, #0x4000, lsl #16    
 orr w1, w1, w2
 str w1, [x3]
 
-movz x4, #1000000000      
+movz x4, #0xFFFF
+movk x4, #0xFFFF, lsl #16     
 delay_loop:
 subs x4, x4, #1         
 bne delay_loop 
 
 ldr w1, x5          
-movz w2, #1, lsl #9       
+movz w2, #0x0 
+movk w2, #0x4000, lsl #16       
 orr w1, w1, w2
 str w1, [x5]
 
-movz x4, #1000000000     
+movz x4, #0xFFFF  
+movk x4, #0xFFFF, lsl #16    
 delay_loop2:
 subs x4, x4, #1         
 bne delay_loop2
 
 b loop
-
-and x0, x0, x0
